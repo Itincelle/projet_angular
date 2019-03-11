441719c376e1eeba0f5f9d01bdfa14699c5ce49c
ayoubM2i <ayo.daif@gmail.com>
2019-03-11 14:43:34 +0100

    fin matiné  avant url relative

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import  { FormsModule } from '@angular/forms';
+import { ProduitService } from 'src/app/produit.service';
+import { HttpClientModule } from '../../node_modules/@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , FormsModule, HttpClientModule
   ],
-  providers: [],
+  providers: [ProduitService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
